2ce534cf33127738e0c327ebd11c02a58e2f1839
Omar <omar.branez@gmail.com>
2021-08-22 15:59:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(inputString) {
+    return inputString[0].toUpperCase() + inputString.slice(1)
+  }
+  
+  static sanitize(inputString) { // " '- " allows whitespace
+    return inputString.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+  
+  static titleize(inputString) {
+    const excludeArray = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"] // lmao i forgot the "the"
+    let wordArray = inputString.split(" ")
+    let newSentence = [] // = wordArray.push
+
+    for (const word of wordArray) {
+      // debugger
+      if (word === wordArray[0]) {
+        newSentence.push(this.capitalize(word)) 
+      } else {
+        if (!(excludeArray.includes(word))) {
+          newSentence.push(this.capitalize(word))
+        } else {
+          newSentence.push(word)
+        }
+      }
+    }
+    return newSentence.join(" ")
+  }
 }
